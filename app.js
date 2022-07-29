134b720875eb183e5141f2aaa1b46f039330b2c8
michaelasong <michaelasong237@gmail.com>
2022-07-29 16:35:29 +0100

    Added interactive Nav menu to mobile version

---
+++ app.js
@@ -0,0 +1,9 @@
+function OpenBar () {
+    var closebtn = document.querySelector(".small-screen");
+    closebtn.style.display = "flex";
+}
+
+function CloseBar(){
+    var closebtn = document.querySelector(".small-screen");
+    closebtn.style.display = "none";
+}
