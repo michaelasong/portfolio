64a7b3b42e3e93d93277ccfe27a1bf433fdeac12
michaelasong <michaelasong237@gmail.com>
2022-07-29 19:33:43 +0100

    Adjustments made and eslint installed

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
-function OpenBar () {
-    var closebtn = document.querySelector(".small-screen");
-    closebtn.style.display = "flex";
+/* eslint-disable no-unused-vars */
+function OpenBar() {
+  const closebtn = document.querySelector('.small-screen');
+  closebtn.style.display = 'flex';
 }
 
-function CloseBar(){
-    var closebtn = document.querySelector(".small-screen");
-    closebtn.style.display = "none";
+function CloseBar() {
+  const closebtn = document.querySelector('.small-screen');
+  closebtn.style.display = 'none';
+}
+
+function showPopUp(a) {
+  const pop = document.getElementById('popUps');
+  pop.style.display = 'flex';
+  if (a === 'tonic') {
+    console.log(a);
+  } else {
+    console.log(a);
+  }
+}
+function closePopUp() {
+  const pop = document.getElementById('popUps');
+  pop.style.display = 'none';
 }
