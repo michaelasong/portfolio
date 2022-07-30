41824994e472bf9d270baf96f2ad35a979c7be79
michaelasong <michaelasong237@gmail.com>
2022-07-30 22:07:54 +0100

    local storage completed

--- app.js
+++ app.js
@@ -1,118 +1,118 @@
 /* eslint-disable no-unused-vars */
 function OpenBar() {
-  const closebtn = document.querySelector(".small-screen");
-  closebtn.style.display = "flex";
+  const closebtn = document.querySelector('.small-screen');
+  closebtn.style.display = 'flex';
 }
 
 function CloseBar() {
-  const closebtn = document.querySelector(".small-screen");
-  closebtn.style.display = "none";
+  const closebtn = document.querySelector('.small-screen');
+  closebtn.style.display = 'none';
 }
 
 const projects = [
   {
-    name: "Tonic",
+    name: 'Tonic',
     description:
-      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas molestias iusto architecto blanditiis evenietimpedit tenetur dolorem, non voluptates atque itaque quaerat praesentium incidunt eligendi nostrum animi harumid commodi.Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium corporis blanditiis similique neque illomaiores vel repudiandae odit? Facere error suscipit laborum a deleniti rerum dicta quas est corrupti",
-    image: "./image/card-1.svg",
-    technologies: ["Html", "Css", "Javascript"],
+      ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas molestias iusto architecto blanditiis evenietimpedit tenetur dolorem, non voluptates atque itaque quaerat praesentium incidunt eligendi nostrum animi harumid commodi.Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium corporis blanditiis similique neque illomaiores vel repudiandae odit? Facere error suscipit laborum a deleniti rerum dicta quas est corrupti',
+    image: './image/card-1.svg',
+    technologies: ['Html', 'Css', 'Javascript'],
   },
   {
-    name: "Multi-Post Stories",
+    name: 'Multi-Post Stories',
     description:
-      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas molestias iusto architecto blanditiis evenietimpedit tenetur dolorem, non voluptates atque itaque quaerat praesentium incidunt eligendi nostrum animi harumid commodi.Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium corporis blanditiis similique neque illomaiores vel repudiandae odit? Facere error suscipit laborum a deleniti rerum dicta quas est corrupti",
-    image: "./image/card-2.svg",
-    technologies: ["Html", "Css", "Javascript"],
+      ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas molestias iusto architecto blanditiis evenietimpedit tenetur dolorem, non voluptates atque itaque quaerat praesentium incidunt eligendi nostrum animi harumid commodi.Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium corporis blanditiis similique neque illomaiores vel repudiandae odit? Facere error suscipit laborum a deleniti rerum dicta quas est corrupti',
+    image: './image/card-2.svg',
+    technologies: ['Html', 'Css', 'Javascript'],
   },
   {
-    name: "Tonic",
+    name: 'Tonic',
     description:
-      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas molestias iusto architecto blanditiis evenietimpedit tenetur dolorem, non voluptates atque itaque quaerat praesentium incidunt eligendi nostrum animi harumid commodi.Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium corporis blanditiis similique neque illomaiores vel repudiandae odit? Facere error suscipit laborum a deleniti rerum dicta quas est corrupti",
-    image: "./image/card-3.svg",
-    technologies: ["Html", "Css", "Javascript"],
+      ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas molestias iusto architecto blanditiis evenietimpedit tenetur dolorem, non voluptates atque itaque quaerat praesentium incidunt eligendi nostrum animi harumid commodi.Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium corporis blanditiis similique neque illomaiores vel repudiandae odit? Facere error suscipit laborum a deleniti rerum dicta quas est corrupti',
+    image: './image/card-3.svg',
+    technologies: ['Html', 'Css', 'Javascript'],
   },
   {
-    name: "Multi-Post Stories",
+    name: 'Multi-Post Stories',
     description:
-      "A Phaser3 platform game built as part of the Microverse JavaScript program.",
-    image: "./image/card-4.svg",
-    technologies: ["Html", "Css", "Javascript"],
+      'A Phaser3 platform game built as part of the Microverse JavaScript program.',
+    image: './image/card-4.svg',
+    technologies: ['Html', 'Css', 'Javascript'],
   },
 ];
 
 function showPopUp(a) {
   // popUp container is created with a class assign
-  const popUps = document.createElement("div");
-  popUps.id = "popUps";
+  const popUps = document.createElement('div');
+  popUps.id = 'popUps';
 
   // popUp content is created with the class assign
-  const popContent = document.createElement("div");
-  popContent.className = "popContent";
+  const popContent = document.createElement('div');
+  popContent.className = 'popContent';
 
   // poUp heading created
-  const popHeading = document.createElement("div");
-  popHeading.className = "popHeading";
+  const popHeading = document.createElement('div');
+  popHeading.className = 'popHeading';
 
-  const title = document.createElement("h1");
-  const close = document.createElement("h3");
+  const title = document.createElement('h1');
+  const close = document.createElement('h3');
 
   title.innerText = projects[a].name;
-  close.innerText = "x";
+  close.innerText = 'x';
 
   popHeading.appendChild(title);
   popHeading.appendChild(close);
 
-  close.addEventListener("click", () => {
-    const pop = document.getElementById("popUps");
+  close.addEventListener('click', () => {
+    const pop = document.getElementById('popUps');
     document.body.removeChild(pop);
   });
 
   // popUp little Description ----------------------------
-  const popDesc = document.createElement("ul");
-  popDesc.className = "popDesc";
-  const items1 = ["CANOPY", "• Back End Dev", "• 2015"];
+  const popDesc = document.createElement('ul');
+  popDesc.className = 'popDesc';
+  const items1 = ['CANOPY', '• Back End Dev', '• 2015'];
 
   for (let i = 0; i < items1.length; i += 1) {
-    const li = document.createElement("li");
+    const li = document.createElement('li');
     li.innerText = items1[i];
     popDesc.appendChild(li);
   }
 
   // pop iMages
-  const image = document.createElement("img");
+  const image = document.createElement('img');
   image.src = projects[a].image;
-  image.className = "popImg";
+  image.className = 'popImg';
 
   // PopDetails
-  const popDetails = document.createElement("div");
-  popDetails.className = "popDetails";
+  const popDetails = document.createElement('div');
+  popDetails.className = 'popDetails';
 
   // pop test description
-  const detail = document.createElement("p");
+  const detail = document.createElement('p');
   detail.innerText =
-    " Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas molestias iusto architecto blanditiis evenietimpedit tenetur dolorem, non voluptates atque itaque quaerat praesentium incidunt eligendi nostrum animi harumid commodi.Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium corporis blanditiis similique neque illomaiores vel repudiandae odit? Facere error suscipit laborum a deleniti rerum dicta quas est corrupti";
-  detail.className = "popDetails";
+    ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas molestias iusto architecto blanditiis evenietimpedit tenetur dolorem, non voluptates atque itaque quaerat praesentium incidunt eligendi nostrum animi harumid commodi.Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium corporis blanditiis similique neque illomaiores vel repudiandae odit? Facere error suscipit laborum a deleniti rerum dicta quas est corrupti';
+  detail.className = 'popDetails';
 
-  const lang = document.createElement("div");
-  lang.className = "lang";
+  const lang = document.createElement('div');
+  lang.className = 'lang';
 
-  const langTags = document.createElement("ul");
-  langTags.className = "langTags";
+  const langTags = document.createElement('ul');
+  langTags.className = 'langTags';
   const items3 = projects[a].technologies;
 
   for (let i = 0; i < items3.length; i += 1) {
-    const li = document.createElement("li");
+    const li = document.createElement('li');
     li.innerText = items3[i];
     langTags.appendChild(li);
   }
 
-  const popBtns = document.createElement("div");
-  popBtns.className = "popBtns";
-  const items4 = ["See Live", "See Source"];
+  const popBtns = document.createElement('div');
+  popBtns.className = 'popBtns';
+  const items4 = ['See Live', 'See Source'];
 
   for (let i = 0; i < items4.length; i += 1) {
-    const li = document.createElement("button");
-    li.className = "seeBtn";
+    const li = document.createElement('button');
+    li.className = 'seeBtn';
     li.innerText = items4[i];
     popBtns.appendChild(li);
   }
@@ -134,8 +134,8 @@ function showPopUp(a) {
 }
 
 function closePopUp() {
-  const pop = document.getElementById("popUps");
-  pop.style.display = "none";
+  const pop = document.getElementById('popUps');
+  pop.style.display = 'none';
 }
 
 // --------------- form validation section ----------------
@@ -143,43 +143,39 @@ function closePopUp() {
 // eslint-disable-next-line consistent-return
 function validateForm() {
   const form = document.forms.form1.mail.value;
-  const err = document.getElementsByClassName("error");
+  const err = document.getElementsByClassName('error');
 
   if (form !== form.toLowerCase()) {
     // eslint-disable-next-line no-alert
-    alert("Wrong email format remove upper case");
+    alert('Wrong email format remove upper case');
     return false;
   }
 }
 
-var formObj = { name: "", email: "", content: "" };
+let formObj = { name: '', email: '', content: '' };
 
 function updateForm() {
-  document.getElementById("text-input").addEventListener("input", (e) => {
+  document.getElementById('text-input').addEventListener('input', (e) => {
     formObj.name = e.target.value;
   });
 
-  document.getElementById("mail").addEventListener("input", (e) => {
+  document.getElementById('mail').addEventListener('input', (e) => {
     formObj.email = e.target.value;
   });
 
-  document.getElementById("msg").addEventListener("input", (e) => {
+  document.getElementById('msg').addEventListener('input', (e) => {
     formObj.content = e.target.value;
   });
 
-  localStorage.setItem("formObj", JSON.stringify(formObj));
-  var retrievedObject = localStorage.getItem("formObj");
-  console.log("retrievedObject: ", JSON.parse(retrievedObject));
+  localStorage.setItem('formObj', JSON.stringify(formObj));
 }
 
 function onLoad() {
   // Retrieve the object from storage
-  var retrievedObject = JSON.parse(localStorage.getItem("formObj"));
-  console.log("retrievedObject: ", retrievedObject);
-  if ( retrievedObject.name ) {
-    document.getElementById("text-input").value = retrievedObject.name;
-    document.getElementById("mail").value = retrievedObject.email;
-    document.getElementById("msg").value = retrievedObject.content;
+  const retrievedObject = JSON.parse(localStorage.getItem('formObj'));
+  if (retrievedObject.name) {
+    document.getElementById('text-input').value = retrievedObject.name;
+    document.getElementById('mail').value = retrievedObject.email;
+    document.getElementById('msg').value = retrievedObject.content;
   }
-
 }
